1a1cdaeb76833609c8b1debd1c84deeac2f56440
CHAO FENG CHAN <peterjan.tw@gmail.com>
2023-06-29 15:22:03 +0800

    Finished editing video and producing output video file.

--- yt_concate/utils.py
+++ yt_concate/utils.py
@@ -4,6 +4,7 @@ from vtt_to_srt.vtt_to_srt import ConvertDirectories
 from yt_concate.settings import DOWNLOADS_DIR
 from yt_concate.settings import VIDEOS_DIR
 from yt_concate.settings import CAPTIONS_DIR
+from yt_concate.settings import OUTPUTS_DIR
 
 class Utils:
     def __init__(self):
@@ -13,6 +14,7 @@ class Utils:
         os.makedirs(DOWNLOADS_DIR, exist_ok=True)
         os.makedirs(VIDEOS_DIR, exist_ok=True)
         os.makedirs(CAPTIONS_DIR, exist_ok=True)
+        os.makedirs(OUTPUTS_DIR, exist_ok=True)
 
     @staticmethod
     def convert_vtt_to_srt():
@@ -49,3 +51,7 @@ class Utils:
     def video_list_file_exists(self, channel_id):
         path = self.get_video_list_filepath(channel_id)
         return os.path.exists(path) and os.path.getsize(path) > 0
+
+    def get_output_filepath(self, channel_id, search_word):
+        filename = channel_id + '_' + search_word + '.mp4'
+        return os.path.join(OUTPUTS_DIR, filename)
